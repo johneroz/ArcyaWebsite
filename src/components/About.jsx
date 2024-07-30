8d07c8998dd6cf48e28dc2990db56aa5b2dacd0e
johneroz <johneroz2122@gmail.com>
2024-07-30 11:44:17 +0800

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ function About() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [modalImage, setModalImage] = useState(null);
 
-  
   const handleImageClick = (imageSrc) => {
     setModalImage(imageSrc);
     setModalOpen(true);
@@ -25,24 +24,30 @@ function About() {
           <h2>Our Story</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida rutrum. Bibendum neque egestas congue quisque egestas diam in arcu. Consequat ac felis donec et odio pellentesque.</p>
         </div>
-        <img
-          src={aboutImage1}
-          alt="About Us 1"
-          className="about-image"
-          onClick={() => handleImageClick(aboutImage1)}
-        />
+        <div className="about-image-container">
+          <img
+            src={aboutImage1}
+            alt="About Us 1"
+            className="about-image"
+            onClick={() => handleImageClick(aboutImage1)}
+          />
+          <span className="photo-credit">Photo by Doobalro TV</span>
+        </div>
       </div>
       <div className="about-content reverse">
         <div className="about-text">
           <h2>History</h2>
           <p>Ultrices dui sapien eget mi proin sed. Adipiscing elit pellentesque habitant morbi. Non sodales neque sodales ut etiam sit amet. Non enim praesent elementum facilisis leo vel fringilla est. Tristique senectus et netus et malesuada. Sed enim ut sem viverra aliquet eget. Sed viverra ipsum nunc aliquet bibendum enim.</p>
         </div>
-        <img
-          src={aboutImage2}
-          alt="About Us 2"
-          className="about-image"
-          onClick={() => handleImageClick(aboutImage2)}
-        />
+        <div className="about-image-container">
+          <img
+            src={aboutImage2}
+            alt="About Us 2"
+            className="about-image"
+            onClick={() => handleImageClick(aboutImage2)}
+          />
+          <span className="photo-credit">Image courtesy of Lamudi</span>
+        </div>
       </div>
       {isModalOpen && (
         <div className="modal" onClick={handleCloseModal}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom'; // Ensure you're using React Router
@@ -6,11 +6,24 @@ import '../styles/Home.css';
 import homeImage from '../assets/Home/homeImage.jpg'; // Replace with your image path
 
 function Home() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+
   const scrollToContent = () => {
     window.scrollTo({
       top: window.innerHeight,
       behavior: 'smooth',
     });
+  };
+
+  const handleImageClick = (imageSrc) => {
+    setModalImage(imageSrc);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setModalImage(null);
   };
 
   return (
@@ -22,17 +35,29 @@ function Home() {
           <FontAwesomeIcon icon={faChevronDown} />
         </div>
       </header>
-      
+
       <section className="home-info">
         <div className="home-info-text">
           <h2>About Us</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida rutrum. Bibendum neque egestas congue quisque egestas diam in arcu. Consequat ac felis donec et odio pellentesque.</p>
           <Link to="/about" className="learn-more">Learn More</Link>
         </div>
-        <div className="home-info-image">
-          <img src={homeImage} alt="Home" />
+        <div className="home-info-image-container" onClick={() => handleImageClick(homeImage)}>
+          <img
+            src={homeImage}
+            alt="Home"
+            className="home-info-image"
+          />
+          <span className="photo-credit">Image courtesy of Tripadvisor</span>
         </div>
       </section>
+
+      {isModalOpen && (
+        <div className="modal" onClick={handleCloseModal}>
+          <span className="close">&times;</span>
+          <img className="modal-content" src={modalImage} alt="Enlarged" />
+        </div>
+      )}
     </>
   );
 }
